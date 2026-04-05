815c2bb14814c967caee57a4b1816cf856e6f03d
Marharyta Prymak <prymak.marharyta@student.sumdu.edu.ua>
2026-04-05 14:12:27 +0300

    Implement saving data to file

--- src/main/java/app/FileManager.java
+++ src/main/java/app/FileManager.java
@@ -62,4 +62,46 @@ public class FileManager {
 
         return books;
     }
+
+    // метод для збереження даних у файл
+    public static void saveToFile(ArrayList<Book> books, String fileName) {
+    try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+
+        for (Book b : books) {
+            String line = "";
+
+            if (b instanceof EBook) {
+                EBook e = (EBook) b;
+                line = "EBook;" + e.getTitle() + ";" + e.getAuthor() + ";" +
+                        e.getYear() + ";" + e.getPrice() + ";" + e.getGenre() +
+                        ";" + e.getPages() + ";" + e.getFileSize();
+            } else if (b instanceof PaperBook) {
+                PaperBook p = (PaperBook) b;
+                line = "PaperBook;" + p.getTitle() + ";" + p.getAuthor() + ";" +
+                        p.getYear() + ";" + p.getPrice() + ";" + p.getGenre() +
+                        ";" + p.getPages() + ";" + p.getCoverType();
+            } else if (b instanceof AudioBook) {
+                AudioBook a = (AudioBook) b;
+                line = "AudioBook;" + a.getTitle() + ";" + a.getAuthor() + ";" +
+                        a.getYear() + ";" + a.getPrice() + ";" + a.getGenre() +
+                        ";" + a.getPages() + ";" + a.getDuration() + ";" + a.getNarrator();
+            } else if (b instanceof UsedBook) {
+                UsedBook u = (UsedBook) b;
+                line = "UsedBook;" + u.getTitle() + ";" + u.getAuthor() + ";" +
+                        u.getYear() + ";" + u.getPrice() + ";" + u.getGenre() +
+                        ";" + u.getPages() + ";" + u.getCondition() + ";" + u.getDiscount();
+            } else {
+                line = "Book;" + b.getTitle() + ";" + b.getAuthor() + ";" +
+                        b.getYear() + ";" + b.getPrice() + ";" + b.getGenre() +
+                        ";" + b.getPages();
+            }
+
+            bw.write(line);
+            bw.newLine();
+        }
+
+    } catch (IOException e) {
+        System.out.println("Помилка запису: " + e.getMessage());
+    }
+}
 }
