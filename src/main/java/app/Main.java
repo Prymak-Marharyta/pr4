efe34b7f662f98c5fae91fb4058fe25f12014722
Marharyta Prymak <prymak.marharyta@student.sumdu.edu.ua>
2026-04-12 00:47:38 +0300

    Refactor Main to use Library instead of ArrayList

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -7,174 +7,61 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        ArrayList<Book> books = JsonManager.load("input.json");
+        ArrayList<Book> loadedBooks = FileManager.loadFromFile("input.txt");
+        
+        Library library = new Library();
+
+        for (Book b : loadedBooks) {
+            library.addNewBook(b, 1);
+        }
 
         System.out.println("=== Система облiку книг ===");
 
-        while (true) {
+         while (true) {
             System.out.println("\n=== Головне меню ===");
             System.out.println("1. Створити новий об’єкт");
-            System.out.println("2. Вивести iнформацiю про всi об’єкти");
-            System.out.println("3. Пошук об’єкта");
-            System.out.println("4. Завершити роботу програми");
+            System.out.println("2. Вивести всi книги");
+            System.out.println("3. Пошук");
+            System.out.println("4. Завершити роботу");
 
             String choice = sc.nextLine().trim();
 
-            try {
-                switch (choice) {
-                    case "1":
-                        createNewObjectMenu(sc, books);
-                        break;
+            switch (choice) {
+                case "1":
+                    createNewObjectMenu(sc, library);
+                    break;
 
-                    case "2":
-                        System.out.println("\n--- Список книг ---");
-                        if (books.isEmpty()) {
-                            System.out.println("Список порожнiй.");
-                        } else {
-                            for (Book b : books) {
-                                System.out.println(b); // ПОЛIФОРМIЗМ
-                            }
-                        }
-                        break;
+                case "2":
+                    library.printAll();
+                    break;
 
-                    case "3":
-                        searchMenu(sc, books);
-                        break;
+                case "3":
+                    searchMenu(sc, library);
+                    break;
 
-                    case "4":
-                        JsonManager.save(books, "input.json");
-                        System.out.println("Дані збережено!");
-                        return;
-                    default:
-                        System.out.println("Невiрний вибiр");
-                }
+                case "4":
+                    FileManager.saveToFile(library.toBookList(), "input.txt");
+                    System.out.println("Данi збережено!");
+                    return;
 
-            } catch (Exception e) {
-                System.out.println("Помилка: " + e.getMessage());
+                default:
+                    System.out.println("Невiрний вибiр");
             }
         }
     }
 
     // меню створення об’єкта
-    private static void createNewObjectMenu(Scanner sc, ArrayList<Book> books) {
-        while (true) {
-            System.out.println("\n--- Створення нового об’єкта ---");
-            System.out.println("1. Book");
-            System.out.println("2. EBook");
-            System.out.println("3. PaperBook");
-            System.out.println("4. AudioBook");
-            System.out.println("5. UsedBook");
-            System.out.println("0. Назад у головне меню");
+    private static void createNewObjectMenu(Scanner sc, Library library) {
 
-            String typeChoice = sc.nextLine().trim();
+        System.out.println("Оберiть тип книги:");
+        System.out.println("1. Book");
+        System.out.println("2. EBook");
+        System.out.println("3. PaperBook");
+        System.out.println("4. AudioBook");
+        System.out.println("5. UsedBook");
 
-            switch (typeChoice) {
-                case "0":
-                    return;
+        String type = sc.nextLine();
 
-                case "1": {
-                    Book book = createBaseBook(sc);
-                    books.add(book);
-                    System.out.println("Додано Book");
-                    return;
-                }
-
-                case "2": {
-                    Book base = createBaseBook(sc);
-                    double fileSize = readDouble(sc, "Розмiр файлу (MB): ", 0, 10000);
-
-                    EBook ebook = new EBook(
-                            base.getTitle(),
-                            base.getAuthor(),
-                            base.getYear(),
-                            base.getPrice(),
-                            base.getGenre(),
-                            base.getPages(),
-                            fileSize
-                    );
-
-                    books.add(ebook);
-                    System.out.println("Додано EBook");
-                    return;
-                }
-
-                case "3": {
-                    Book base = createBaseBook(sc);
-
-                    System.out.print("Тип обкладинки: ");
-                    String cover = sc.nextLine();
-
-                    PaperBook pb = new PaperBook(
-                            base.getTitle(),
-                            base.getAuthor(),
-                            base.getYear(),
-                            base.getPrice(),
-                            base.getGenre(),
-                            base.getPages(),
-                            cover
-                    );
-
-                    books.add(pb);
-                    System.out.println("Додано PaperBook");
-                    return;
-                }
-
-                case "4": {
-                    Book base = createBaseBook(sc);
-
-                    int duration = readInt(sc, "Тривалiсть (хв): ", 1, 10000);
-
-                    System.out.print("Диктор: ");
-                    String narrator = sc.nextLine();
-
-                    AudioBook ab = new AudioBook(
-                            base.getTitle(),
-                            base.getAuthor(),
-                            base.getYear(),
-                            base.getPrice(),
-                            base.getGenre(),
-                            base.getPages(),
-                            duration,
-                            narrator
-                    );
-
-                    books.add(ab);
-                    System.out.println("Додано AudioBook");
-                    return;
-                }
-
-                case "5": {
-                    Book base = createBaseBook(sc);
-
-                    System.out.print("Стан книги: ");
-                    String condition = sc.nextLine();
-
-                    double discount = readDouble(sc, "Знижка (%): ", 0, 100);
-
-                    UsedBook ub = new UsedBook(
-                            base.getTitle(),
-                            base.getAuthor(),
-                            base.getYear(),
-                            base.getPrice(),
-                            base.getGenre(),
-                            base.getPages(),
-                            condition,
-                            discount
-                    );
-
-                    books.add(ub);
-                    System.out.println("Додано UsedBook");
-                    return;
-                }
-
-                default:
-                    System.out.println("Невiрний вибiр типу. Спробуйте ще раз.");
-            }
-        }
-    }
-
-    // cтворення базового об'єкта Book
-    private static Book createBaseBook(Scanner sc) {
         System.out.print("Назва: ");
         String title = sc.nextLine();
 
@@ -190,13 +77,104 @@ public class Main {
                 System.out.println("Жанр (FICTION, SCIENCE, HISTORY, FANTASY, DETECTIVE): ");
                 genre = Genre.valueOf(sc.nextLine().trim().toUpperCase());
             } catch (Exception e) {
-                System.out.println("Невiрний жанр. Спробуйте ще раз.");
+                System.out.println("Помилка жанру");
             }
         }
 
         int pages = readInt(sc, "Сторiнки: ", 1, 10000);
 
-        return new Book(title, author, year, price, genre, pages);
+     switch (type) {
+
+        case "1": {
+            Book book = new Book(title, author, year, price, genre, pages);
+            library.addNewBook(book, 1);
+            break;
+        }
+
+        case "2": {
+            double size = readDouble(sc, "Розмiр файлу: ", 0, 10000);
+            Book book = new EBook(title, author, year, price, genre, pages, size);
+            library.addNewBook(book, 1);
+            break;
+        }
+
+        case "3": {
+            System.out.print("Тип обкладинки: ");
+            String cover = sc.nextLine();
+            Book book = new PaperBook(title, author, year, price, genre, pages, cover);
+            library.addNewBook(book, 1);
+            break;
+        }
+
+        case "4": {
+            int duration = readInt(sc, "Тривалiсть: ", 1, 10000);
+            System.out.print("Диктор: ");
+            String narrator = sc.nextLine();
+            Book book = new AudioBook(title, author, year, price, genre, pages, duration, narrator);
+            library.addNewBook(book, 1);
+            break;
+        }
+
+        case "5": {
+            System.out.print("Стан: ");
+            String condition = sc.nextLine();
+            double discount = readDouble(sc, "Знижка: ", 0, 100);
+            Book book = new UsedBook(title, author, year, price, genre, pages, condition, discount);
+            library.addNewBook(book, 1);
+            break;
+        }
+
+        default:
+            System.out.println("Невiрний вибiр");
+    }
+
+    System.out.println("Книгу додано!");
+}
+
+private static void searchMenu(Scanner sc, Library library) {
+
+    while (true) {
+        System.out.println("\n--- Пошук ---");
+        System.out.println("1. За назвою");
+        System.out.println("2. За автором");
+        System.out.println("3. За роком");
+        System.out.println("0. Назад");
+
+        String choice = sc.nextLine().trim();
+
+        switch (choice) {
+            case "0":
+                return;
+
+            case "1":
+                System.out.print("Введiть назву: ");
+                printResults(library.searchByTitle(sc.nextLine()));
+                break;
+
+            case "2":
+                System.out.print("Введiть автора: ");
+                printResults(library.searchByAuthor(sc.nextLine()));
+                break;
+
+            case "3":
+                int year = readInt(sc, "Введiть рiк: ", 0, 3000);
+                printResults(library.searchByYear(year));
+                break;
+
+            default:
+                System.out.println("Невiрний вибiр");
+        }
+    }
+}
+
+    private static void printResults(ArrayList<Book> result) {
+        if (result.isEmpty()) {
+            System.out.println("Нiчого не знайдено");
+        } else {
+            for (Book b : result) {
+                System.out.println(b);
+            }
+        }
     }
 
     // метод для безпечного читання int з перевіркою діапазону
@@ -223,90 +201,6 @@ public class Main {
                 return value;
             } catch (Exception e) {
                 System.out.println("Помилка вводу");
-            }
-        }
-    }
-
-    private static void searchMenu(Scanner sc, ArrayList<Book> books) {
-    while (true) {
-        System.out.println("\n--- Пошук ---");
-        System.out.println("1. За назвою");
-        System.out.println("2. За автором");
-        System.out.println("3. За роком");
-        System.out.println("0. Назад");
-
-        String choice = sc.nextLine().trim();
-
-        switch (choice) {
-            case "0":
-                return;
-
-            case "1":
-                System.out.print("Введiть назву: ");
-                String title = sc.nextLine();
-                printResults(searchByTitle(books, title));
-                break;
-
-            case "2":
-                System.out.print("Введiть автора: ");
-                String author = sc.nextLine();
-                printResults(searchByAuthor(books, author));
-                break;
-
-            case "3":
-                int year = readInt(sc, "Введiть рiк: ", 0, 3000);
-                printResults(searchByYear(books, year));
-                break;
-
-            default:
-                System.out.println("Невiрний вибiр");
-        }
-    }
-}
-
-    private static ArrayList<Book> searchByTitle(ArrayList<Book> books, String title) {
-        ArrayList<Book> result = new ArrayList<>();
-
-        for (Book b : books) {
-            if (b.getTitle().toLowerCase().contains(title.toLowerCase())) {
-            result.add(b);
-            }
-        }
-
-        return result;
-    }
-
-    private static ArrayList<Book> searchByAuthor(ArrayList<Book> books, String author) {
-        ArrayList<Book> result = new ArrayList<>();
-
-        for (Book b : books) {
-            if (b.getAuthor().equalsIgnoreCase(author)) {
-            result.add(b);
-            }
-        }
-
-        return result;
-    }
-
-    private static ArrayList<Book> searchByYear(ArrayList<Book> books, int year) {
-        ArrayList<Book> result = new ArrayList<>();
-
-        for (Book b : books) {
-            if (b.getYear() == year) {
-            result.add(b);
-            }
-        }
-
-    return result;
-    }
-
-    private static void printResults(ArrayList<Book> result) {
-        if (result.isEmpty()) {
-            System.out.println("Нiчого не знайдено");
-        } else {
-            System.out.println("\n--- Результати пошуку ---");
-            for (Book b : result) {
-                System.out.println(b);
             }
         }
     }
