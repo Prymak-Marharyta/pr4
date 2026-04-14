4a839c7da9a1cc0337509bbbe7c39e851b53ce0d
Marharyta Prymak <prymak.marharyta@student.sumdu.edu.ua>
2026-04-14 22:26:07 +0300

    Add insert logic using PreparedStatement

--- src/main/java/app/DatabaseManager.java
+++ src/main/java/app/DatabaseManager.java
@@ -4,7 +4,10 @@ import java.sql.*;
 import java.util.Properties;
 import java.io.FileInputStream;
 
-public DatabaseManager(String configPath) {
+public class DatabaseManager {
+    private Connection connection;
+
+    public DatabaseManager(String configPath) {
     try {
         Properties props = new Properties();
         FileInputStream fis = new FileInputStream(configPath);
@@ -19,5 +22,58 @@ public DatabaseManager(String configPath) {
     } catch (Exception e) {
         System.out.println("Помилка підключення до БД");
         e.printStackTrace();
+    }
+}
+
+public void insertBook(Book book) {
+    String sql = "INSERT INTO books (type, title, author, year, price, genre, pages, size, cover, duration, narrator, condition, discount) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    try {
+        PreparedStatement ps = connection.prepareStatement(sql);
+
+        // type
+        ps.setString(1, book.getClass().getSimpleName());
+
+        // базові поля
+        ps.setString(2, book.getTitle());
+        ps.setString(3, book.getAuthor());
+        ps.setInt(4, book.getYear());
+        ps.setDouble(5, book.getPrice());
+        ps.setString(6, book.getGenre().toString());
+        ps.setInt(7, book.getPages());
+
+        // додаткові (по дефолту null)
+        ps.setObject(8, null);
+        ps.setObject(9, null);
+        ps.setObject(10, null);
+        ps.setObject(11, null);
+        ps.setObject(12, null);
+        ps.setObject(13, null);
+
+        // перевірка підкласів
+        if (book instanceof EBook) {
+            ps.setDouble(8, ((EBook) book).getSize());
+        }
+
+        if (book instanceof PaperBook) {
+            ps.setString(9, ((PaperBook) book).getCover());
+        }
+
+        if (book instanceof AudioBook) {
+            ps.setInt(10, ((AudioBook) book).getDuration());
+            ps.setString(11, ((AudioBook) book).getNarrator());
+        }
+
+        if (book instanceof UsedBook) {
+            ps.setString(12, ((UsedBook) book).getCondition());
+            ps.setDouble(13, ((UsedBook) book).getDiscount());
+        }
+
+        ps.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Помилка INSERT");
+            e.printStackTrace();
+        }
     }
 }
